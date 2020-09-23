9c7369131d3eab6564a79023d7c8d428b8b70eae
Color_yr <402067010@qq.com>
2020-09-23 17:27:04 +0800

    加点注释

--- Src/src/main.cpp
+++ Src/src/main.cpp
@@ -3,6 +3,7 @@
 
 void Taska(void *data)
 {
+    //循环闪烁
     for (;;)
     {
         digitalWrite(LED, HIGH);
@@ -14,9 +15,17 @@ void Taska(void *data)
 
 void setup()
 {
+    //设置引脚模式
     pinMode(LED, OUTPUT);
-    //堆太小会溢出
-    xTaskCreate(Taska, "TaskA", 8192, NULL, 2, NULL);
+    //创建任务
+    xTaskCreate(
+        Taska,   //任务
+        "TaskA", //任务名字
+        8192,    //堆大小，太小会溢出
+        NULL,    //参数
+        2,       //优先级
+        NULL     //控制指针
+    );
 }
 
 void loop()
